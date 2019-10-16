4b5e7d0f1a158a27cd92f09cb8f274eda11e2fa6
Weezer <ewint3@gmail.com>
2019-10-15 19:21:44 -0500

    linking to mysql

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  
+  port: 3306,
+
+
+  user: "ewin",
+
+  
+  password: "iluvbama79",
+  database: "bamazon_db"
+});
