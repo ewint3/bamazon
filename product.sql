91bda3a79bf7d2f324884cace90bdc8030e30cd3
Weezer <ewint3@gmail.com>
2020-01-11 10:44:54 -0600

    fixed sql seed

--- product.sql
+++ product.sql
@@ -13,3 +13,15 @@ CREATE TABLE products (
 );
 
 SELECT * FROM products;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("UGA Hoodie", "Clothing", 29.99, 99),
+("Spider-Man", "Collectibles", 19.99, 144),
+("Micheal Jordan Card", "Collectibles", 9.99, 101),
+("Destiny ps4", "Entertainment", 19.99, 66),
+("Atlanta Falcons Blanket", "Bedding", 24.99, 500),
+("Wolverine Action Figure", "Toys", 5.99, 100),
+("Call of Duty", "Entertainment", 64.99, 181),
+("Mr Coffee Coffee Maker", "Home Goods", 24.99, 6),
+("NorthFace Fleece", "Clothing", 99.99, 69),
+("PS4", "Electronics", 299.99, 77)
